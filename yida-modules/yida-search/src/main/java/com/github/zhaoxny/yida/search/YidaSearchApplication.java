ca6efd68ca1f586f7efb15ffa96effa7183eb5b8
henry <347910189@qq.com>
2021-04-03 09:41:50 +0800

    调整

--- yida-modules/yida-search/src/main/java/com/github/zhaoxny/yida/search/YidaSearchApplication.java
+++ yida-modules/yida-search/src/main/java/com/github/zhaoxny/yida/search/YidaSearchApplication.java
@@ -2,8 +2,11 @@ package com.github.zhaoxny.yida.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@SpringBootApplication
+@EnableElasticsearchRepositories(basePackages = "com.github.zhaoxny.yida.search.repository")
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class YidaSearchApplication {
 
     public static void main(String[] args) {
--- yida-modules/yida-search/src/main/java/com/github/zhaoxny/yida/search/config/MyBatisConfig.java
+++ yida-modules/yida-search/src/main/java/com/github/zhaoxny/yida/search/config/MyBatisConfig.java
@@ -2,12 +2,14 @@ package com.github.zhaoxny.yida.search.config;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * MyBatis相关配置
  * @Author Henry on 2019/4/8.
  */
 @Configuration
-@MapperScan({"com.github.zhaoxny.yida.*.mapper","com.github.zhaoxny.yida.*.dao"})
+@EnableTransactionManagement
+@MapperScan({"com.github.zhaoxny.yida.search.*"})
 public class MyBatisConfig {
 }
