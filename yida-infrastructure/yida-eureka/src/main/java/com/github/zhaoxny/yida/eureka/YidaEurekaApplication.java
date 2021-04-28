08152201f66e9bdb24c91c9df628eea564746693
henry <347910189@qq.com>
2021-04-28 15:04:12 +0800

    fix:添加eureka

---
+++ yida-infrastructure/yida-eureka/src/main/java/com/github/zhaoxny/yida/eureka/YidaEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.github.zhaoxny.yida.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YidaEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YidaEurekaApplication.class, args);
+    }
+
+}
