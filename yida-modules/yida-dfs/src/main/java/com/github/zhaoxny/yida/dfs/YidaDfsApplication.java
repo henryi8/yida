c4b7c8628c870eecddffeafcf74d9909bc9b36c8
henry <347910189@qq.com>
2021-05-10 19:27:59 +0800

    fix:文件系统

---
+++ yida-modules/yida-dfs/src/main/java/com/github/zhaoxny/yida/dfs/YidaDfsApplication.java
@@ -0,0 +1,13 @@
+package com.github.zhaoxny.yida.dfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YidaDfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YidaDfsApplication.class, args);
+    }
+
+}
---
+++ yida-modules/yida-dfs/src/test/java/com/github/zhaoxny/yida/dfs/YidaDfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zhaoxny.yida.dfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YidaDfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
