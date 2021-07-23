dfaad69c83de449e33269e90c94a5e0a23696bf3
henry <347910189@qq.com>
2021-07-23 10:14:34 +0800

    fix:swagger通用组件

---
+++ yida-common/yida-swagger2-spring-boot-starter/src/main/java/com/github/zhaoxny/yida/swagger2/annotation/EnableCustomSwagger2.java
@@ -0,0 +1,15 @@
+package com.github.zhaoxny.yida.swagger2.annotation;
+
+import com.github.zhaoxny.yida.swagger2.config.SwaggerAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({SwaggerAutoConfiguration.class})
+public @interface EnableCustomSwagger2 {
+
+}
