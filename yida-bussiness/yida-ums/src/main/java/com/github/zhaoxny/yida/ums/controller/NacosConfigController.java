287fd1104f5b2b7014a279bb354127d0083ab2d6
henry <347910189@qq.com>
2021-07-29 13:40:13 +0800

    fix：修改配置

---
+++ yida-bussiness/yida-ums/src/main/java/com/github/zhaoxny/yida/ums/controller/NacosConfigController.java
@@ -0,0 +1,21 @@
+package com.github.zhaoxny.yida.ums.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class NacosConfigController {
+
+//    @Value("${config.env}")
+    public String env;
+    	
+    
+    @GetMapping("/nacos-config")
+    public 	String nacosConfig() {
+    	
+     return  env;
+    }
+}
