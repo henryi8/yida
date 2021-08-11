3fc6a0510bee797bbf6817ad199629779d16d18a
henry <347910189@qq.com>
2021-08-11 16:55:13 +0800

    fix:

--- yida-bussiness/yida-user/src/main/java/com/github/zhaoxny/yida/user/YidaUserApplication.java
+++ yida-bussiness/yida-user/src/main/java/com/github/zhaoxny/yida/user/YidaUserApplication.java
@@ -4,6 +4,7 @@ import com.github.zhaoxny.yida.auth.annotation.EnableYdFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 认证授权中心
@@ -11,7 +12,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @Author Henry
  */
 @EnableYdFeignClients
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@ComponentScan(basePackages ={"com.github.zhaoxny.yida.api" })
 public class YidaUserApplication {
 
     public static void main(String[] args) {
